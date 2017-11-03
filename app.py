f0b50f53db908e2ab2cf2e28b434416e695a7952
Mark Longair <mhl@pobox.com>
2017-11-03 08:04:09 +0000

    Add checks on 'follows' + 'followed by' consistency

--- app.py
+++ app.py
@@ -102,27 +102,111 @@ class Episode(object):
         self.episode_number = int_if_present(binding, 'episodeNumber')
         self.production_code = str_if_present(binding,'productionCode')
         self.previous_episode_item = id_if_present(binding, 'previousEpisode')
+        self.previous_episode = None
         self.next_episode_item = id_if_present(binding, 'nextEpisode')
+        self.next_episode = None
         self.episodes_in_season = int_if_present(binding, 'episodesInSeason')
         self.total_seasons = int_if_present(binding, 'totalSeasons')
+
+    def __eq__(self, other):
+        return self.item == other.item
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 
 def parse_episodes(result_bindings):
     all_episodes = [Episode(b) for b in result_bindings]
+    item_id_to_episode = {}
+    for episode in all_episodes:
+        item_id_to_episode[episode.item] = episode
+    problems = []
+    for episode in all_episodes:
+        if episode.previous_episode_item:
+            if episode.previous_episode_item in item_id_to_episode:
+                episode.previous_episode = item_id_to_episode[episode.previous_episode_item]
+            else:
+                fmt = '{0} follows {1}, but {1} was not found by the query'
+                problems.append(fmt.format(episode.item, episode.previous_episode_item))
+        if episode.next_episode_item:
+            if episode.next_episode_item in item_id_to_episode:
+                episode.next_episode = item_id_to_episode[episode.next_episode_item]
+            else:
+                fmt = '{0} followed by {1} but {1} was not found by the query'
+                problems.append(fmt.format(episode.item, episode.next_episode_item))
     return all_episodes
 
 
 def group_and_order_episodes(episodes):
-    return groupby(episodes, lambda e: (e.season_item, e.season_number, e.season_label))
+    id_to_episode = {}
+    first_episodes = []
+    last_episodes = []
+    for episode in episodes:
+        id_to_episode[episode.item] = episode
+    # Check that the previous and next episodes are consistent:
+    unlinked_episodes = []
+    problems = []
+    for episode in episodes:
+        if not episode.previous_episode_item and not episode.next_episode_item:
+            fmt = 'Episode {item_id} has no previous or next episode'
+            problems.append(fmt.format(item_id=episode.item))
+            unlinked_episodes.append(episode)
+        if episode.previous_episode:
+            if episode.previous_episode.next_episode != episode:
+                fmt = '{0} follows {1}, but {1} followed by {2}'
+                problems.append(fmt.format(
+                    episode.item,
+                    episode.previous_episode.item,
+                    episode.previous_episode.next_episode.item))
+        if episode.next_episode:
+            if episode.next_episode.previous_episode:
+                if episode.next_episode.previous_episode != episode:
+                    fmt = '{0} followed by {1}, but {1} follows {2}'
+                    problems.append(fmt.format(
+                        episode.item,
+                        episode.next_episode.item,
+                        episode.next_episode.previous_episode.item))
+            else:
+                fmt = '{0} followed by {1}, but {1} follows nothing'
+                problems.append(fmt.format(
+                    episode.item,
+                    episode.next_episode.item))
+        if episode.next_episode and not episode.previous_episode:
+            first_episodes.append(episode)
+        if episode.previous_episode and not episode.next_episode:
+            last_episodes.append(episode)
+    first_or_last_problems = []
+    if len(first_episodes) == 0:
+        first_or_last_problems.append('There was no first episode found (in the sense that it has no \'follows\' but does have a \'followed by\'')
+    elif len(first_episodes) > 1:
+        fmt = 'More than one episode had a \'followed by\' but no \'follows\': {0}'
+        first_or_last_problems.append(fmt.format(', '.join(e.label_with_item for e in first_episodes)))
+    if len(last_episodes) == 0:
+        first_or_last_problems.append('There was no last episode found (in the sense that it has no \'followed by\' but does have a \'follows\'')
+    elif len(last_episodes) > 1:
+        fmt = 'More than one episode had a \'follows\' but no \'followed by\': {0}'
+        first_or_last_problems.append(fmt.format(', '.join(e.label_with_item for e in last_episodes)))
+    # If there are no unlinked episodes, 1 first episode and no
+    # 'follows' / 'followed by' consistency problems, then we can
+    # order by the 'follows' / 'followed by' relationships:
+    if len(first_episodes) == 1 and len(unlinked_episodes) == 0 \
+       and len(problems) == 0:
+        episodes = []
+        current_episode = next(iter(first_episodes))
+        while current_episode:
+            episodes.append(current_episode)
+            current_episode = current_episode.next_episode
+    report_items = [(False, problem) for problem in first_or_last_problems + problems]
+    return groupby(episodes, lambda e: (e.season_item, e.season_number, e.season_label)), report_items
 
 
 def problem_report(episodes):
-    report_items = []
+    grouped_episodes_iter, report_items = group_and_order_episodes(episodes)
     # Just make this non-lazy to avoid confusion when items are
     # consumed and you can't get them back:
     grouped = [
         (season_tuple, list(episodes_group))
-        for season_tuple, episodes_group in group_and_order_episodes(episodes)
+        for season_tuple, episodes_group in grouped_episodes_iter
     ]
     for season_tuple, episodes_group in grouped:
         season_item, season_number, season_label = season_tuple
@@ -548,7 +632,7 @@ def random_episode(wikidata_item):
                 all_episodes_query=query,
                 title='No episodes found of {0}'.format(series_name),
             )
-    episodes_table_data = group_and_order_episodes(episodes)
+    episodes_table_data, _ = group_and_order_episodes(episodes)
     episode = random.choice(episodes)
     return render_template(
         'random-episode.html',
